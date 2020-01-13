92998c0ca03d358d7cfe1c0ae4939fca08c8c7bd
Audrey Pierillas <ambrecielle@gmail.com>
2020-01-13 22:47:38 +0100

    phrases +

--- js/swgenerator.js
+++ js/swgenerator.js
@@ -1,56 +1,68 @@
 
 
 var planet = ['Aargonar', 'Abafar', 'Abregado-rae', 'Adumar', 'Agamar', 'Ahch-To', 'Ajan Kloss', 'Ajara', 'Alderaan', 'Aleen', 'Almania', 'Ambria', 'Anaxes', 'Ando', 'l\'Anneau de Kafrene', 'Anoat', 'Antar IV', 'Aria Prime', 'Atollon', 'Atzerri', 'Bahryn', 'Bakura', 'Balamak', 'Balmorra', 'Balnab', 'Bandomeer', 'BArdotta', 'Bastion', 'Batuu', 'Berzite', 'Bespin', 'Bilbringi', 'Bogden', 'Bonadan', 'Borleias', 'Bothawui', 'Brentaal IV', 'Brodo Asogi', 'Byss', 'Cantonica', 'Carida', 'Cato Neimoidia', 'Chandel', 'Chandrila', 'Christophsis', 'Commenor', 'Concord Dawn', 'Concord Dawn III', 'Corellia', 'Corulag', 'Coruscant', 'Crait', 'Csilla', 'Dagobah', 'Dantooine', 'Dathomir', 'Denon', 'Devaron', 'Dorin', 'D\'Qar', 'Dromund Kaas', 'Duro', 'Dxun', 'Eadu', 'Endor', 'la lune forestière d\'Endor', 'Eriadu', 'Er\'Kit', 'Espinar', 'Etti IV', 'Exegol', 'Falleen', 'Felucia', 'Fest', 'Florrum', 'Fondor', 'Garel', 'Garqi', 'Géonosis', 'Ghorman', 'Giju', 'Glee Anselm', 'Gyndine', 'Hapes', 'Honogr', 'Hoth', 'Hosnian Prime', 'Humbarine', 'Iakar', 'Ibaar', 'Iego', 'Ilum', 'Iokath', 'Iridonia', 'Ithor', 'Jabiim', 'Jakku', 'Jalindi', 'Jedha', 'Jelucan', 'KAdavo', 'Kalarba', 'Kalee', 'Kalevala', 'Kamino', 'Kashyyyk', 'Kef Bir', 'Kessel','Kiffex', 'Kiffu', 'Kijimi', 'Killun', 'Kinyen', 'Kiros', 'Kirrek', 'Klatooine', 'Kothlis', 'Krownest', 'Kuat', 'Kubindi', 'Ladro', 'Lah\'mu', 'Lasan', 'Lexrul', 'Lira San', 'Lothal', 'Lotho Minor', 'Malachor', 'Malastare', 'Manaan', 'Mandalore', 'Maridun', 'Milvayne', 'Mimban', 'Mirial', 'Mon Calamari', 'Montross', 'Moraband', 'Mortis', 'Mustafar', 'Muunilinst', 'Mygeeto', 'Mykapo', 'Myrkr', 'Naboo', 'NaJedha', 'Nal Hutta', 'Nam Chorios', 'Nar Shadaa', 'Naraka', 'Nixus', 'Obroa-skai', 'Odessen', 'Onderon', 'Oosalon', 'Ord Mantell', 'Orto Plutonia', 'Ossus', 'Palanhi', 'Pantora', 'Parnassos', 'Pasaana', 'Pipada', 'la planète de la Force', 'Polis Massa', 'Quarzite', 'Quell', 'Rakata Prime', 'Ralltiir', 'Rattatak', 'Raxus Prime', 'Raydonia', 'Rendili', 'Rishi', 'la lune de Rishi', 'Rodia', 'Rudrig', 'Rugosa', 'Ruusan', 'Ryloth', 'Saleucami', 'Sarrish', 'Savareen', 'Scarif', 'Scipio', 'Sebaddon', 'Seelos', 'Selonia', 'Serenno', 'Sernpidal', 'Shantipole', 'Shili', 'le glacier de Sinta', 'Sriluur', 'la station de combat Starkiller', 'Stewjon', 'Stygeon Prime', 'Subterrel', 'Sullust', 'Sulon', 'Taanab', 'Takobo', 'Takodana', 'Talus', 'Talvarin', 'Tangenine', 'Taris', 'Tatooine', 'Telos', 'Telos IV', 'Telti', 'Tepasi', 'Terrelia', 'Teth','Thyferra', 'Tibrin', 'Tinnel IV', 'Tirahnn', 'Toydaria', 'Tralus', 'Trandosha', 'Tython', 'Umbara', 'Utapau', 'Uyter', 'Vallt', 'Vassek', 'Vanqor', 'Varl', 'Velusia', 'Vjun', 'Vortex', 'Voss', 'Wayland', 'Wobani', 'Wynkahthu', 'Yaga Minor', 'Yag\'Dhul', 'Yavin', 'Yavin IV', 'Yinchorr', 'Ylesia', 'Zygerria', 'Zeffo', 'Zakel', 'Ziost', 'Zonama Sekot'];
-
 var randomPlanet = planet[Math.floor(Math.random() * planet.length)];;
 
 
-
 var majorCharacter = ['Luke Skywalker', 'Leia Organa', 'Han Solo', 'Anakin Skywalker', 'Darth Vader', 'Padme Amidala', 'Ben Solo', 'Kylo Ren', 'Poe Dameron', 'l\'Amiral Ackbar', 'Jyn Erso', 'Lando Calrissian', 'Chewbacca', 'Boba Fett', 'Jango Fett', 'le Mandalorien', 'Armitage Hux', 'Mara Jade', 'Qui-Gon Jinn', 'Obi-Wan Kenobi', ];
-
 var randomMajorCharacter = majorCharacter[Math.floor(Math.random() * majorCharacter.length)];;
 
 
-
-var secondaryCharacter = ['Stass Allie', 'Mas Amedda', 'Wedge Antilles', 'Nom Anor', 'Ask Aak', 'Raymus Antilles', 'la reine Apailana', 'Passel Argente', 'Ello Asty', 'Jar-jar Binks', 'Ponda Baba', 'Hermione Bagwa', 'Bala-Tik', 'Darth Bane', 'Kirster Banai','Barada', 'Sio Bibble', 'Depa Billaba', 'Walex Blissex', 'Bobbajo', 'Bossk', 'MAjor Taslin Brance', 'Ezra Bridger', 'Joruus C\'Baoth', 'Chirpa', 'Airen Cracken', 'Salacious Crumb', 'Yarna D\'Al\'Gargan', 'Figrin d\'An', 'Vober Dand', 'Biggs Darklighter', 'Lexi Dio', 'Lott Dod', 'Jan Dodonna', 'Daultay Dofine', 'Dooku', 'Cin Drallig', 'Kyp Durron', 'Hugo Eckener', 'Caluan Ematt', 'Cornelius Evazan', 'Sun Fac', 'Soontir Fel', 'Kit Fisto', 'Fode et Beed', 'Bib Fortuna', 'Silver Fyre', 'Adi Gallia','Garindan', 'Gragra', 'Greeata', 'Greedo', 'Griveous', 'Grummgar', 'Nute Gunray', 'Guri', 'Rune Haako', 'San Hill', 'Corran Horn', 'Kanan Jarrus', 'l\'Inquisiteur', 'Ysanne Isard', 'Isolder', 'Sidon Ithano', 'J\'Quille', 'Jabba le Hutt', 'la reine Jamillia', 'Tiaan Jerjerrod', 'Dexter Jettster', 'Jira', 'Zett Jukassa', 'Harter Kalonia', 'Maz Kanata', 'Kir Kanos', 'Volfe Karkko', 'Talon Karrde', 'Kyle Katarn', 'Ki-Adi-Mundi', 'Agen Kolar', 'Plo Koon', 'Eeth Koth', 'Labria', 'Tsavong Lah', 'Owen Lars', 'Beru Lars', 'Cliegg Lars', 'Tasu Leech', 'Bevel Lemelisk', 'Lobot', 'Logray', 'Paploo', 'Teebo', 'Crix Madine', 'Shu Mai', 'Malakili', 'Darth Maul', 'Droopy McCool', 'Lyn Me', 'Tion Medon', 'Tionne', 'Dopheld Mitaka', 'Ephant Mon', 'Mon Mothma', 'Sly Moore', ];  
-
+var secondaryCharacter = ['Stass Allie', 'Mas Amedda', 'Wedge Antilles', 'Nom Anor', 'Ask Aak', 'Raymus Antilles', 'la reine Apailana', 'Passel Argente', 'Ello Asty', 'Jar-jar Binks', 'Ponda Baba', 'Hermione Bagwa', 'Bala-Tik', 'Darth Bane', 'Kirster Banai','Barada', 'Sio Bibble', 'Depa Billaba', 'Walex Blissex', 'Bobbajo', 'Bossk', 'MAjor Taslin Brance', 'Ezra Bridger', 'Joruus C\'Baoth', 'Chirpa', 'Airen Cracken', 'Salacious Crumb', 'Yarna D\'Al\'Gargan', 'Figrin d\'An', 'Vober Dand', 'Biggs Darklighter', 'Lexi Dio', 'Lott Dod', 'Jan Dodonna', 'Daultay Dofine', 'Dooku', 'Cin Drallig', 'Kyp Durron', 'Hugo Eckener', 'Caluan Ematt', 'Cornelius Evazan', 'Sun Fac', 'Soontir Fel', 'Kit Fisto', 'Fode et Beed', 'Bib Fortuna', 'Silver Fyre', 'Adi Gallia','Garindan', 'Gragra', 'Greeata', 'Greedo', 'Griveous', 'Grummgar', 'Nute Gunray', 'Guri', 'Rune Haako', 'San Hill', 'Corran Horn', 'Kanan Jarrus', 'l\'Inquisiteur', 'Ysanne Isard', 'Isolder', 'Sidon Ithano', 'J\'Quille', 'Jabba le Hutt', 'la reine Jamillia', 'Tiaan Jerjerrod', 'Dexter Jettster', 'Jira', 'Zett Jukassa', 'Harter Kalonia', 'Maz Kanata', 'Kir Kanos', 'Volfe Karkko', 'Talon Karrde', 'Kyle Katarn', 'Ki-Adi-Mundi', 'Agen Kolar', 'Plo Koon', 'Eeth Koth', 'Labria', 'Tsavong Lah', 'Owen Lars', 'Beru Lars', 'Cliegg Lars', 'Tasu Leech', 'Bevel Lemelisk', 'Lobot', 'Logray', 'Paploo', 'Teebo', 'Crix Madine', 'Shu Mai', 'Malakili', 'Darth Maul', 'Droopy McCool', 'Lyn Me', 'Tion Medon', 'Tionne', 'Dopheld Mitaka', 'Ephant Mon', 'Mon Mothma', 'Sly Moore'];  
 var randomSecondaryCharacter = secondaryCharacter[Math.floor(Math.random() * secondaryCharacter.length)];;
 
 
-
 var droid = ['C-3PO', 'R2-D2', 'BB-8', 'EV-9D9', '8D8', 'IG-88', 'ME-8D9'];
-
 var randomDroid = droid[Math.floor(Math.random() * droid.length)];
 
 
 var object = ['un sabre-laser', 'une vieille barre chocolatée infusée de Force', 'une chaussette orpheline', 'l\'intelligence perdue des Jedi, tombée derrière une machine à café', ];
-
 var randomObject = object[Math.floor(Math.random() * object.length)];
 
 
-
 var action = ['d\'une course-poursuite acharnée', 'd\'un duel à mort', 'd\'une soirée mousse vraiment cool', 'd\'un thé dansant endiablé', 'd\'un bal-musette sur la planète Party Hard', 'd\'une grosse cuite au temple jedi', 'd\'un repas à la bonne franquette', 'd\'une journée comme les autres'];
-
 var randomAction = action[Math.floor(Math.random() * action.length)];
 
 
-
 var situation = ['être l\'enfant caché de', 'être le descendant de', 'l\'existence d\'une prophétie impliquant', 'le nombre impressionnant de midi-chloriens dans l\'organisme de', 'un énième clone de l\'Empereur fabriqué par', 'que pour rétablir l\'Equilibre dans la Force il faut donner la moitié de ses revenus à Raël par l\'intermédiaire de'];
-
 var randomSituation = situation[Math.floor(Math.random() * situation.length)];
 
 
 var huttName = ['Zumba', 'Rumba', 'Java', 'JavaScript', 'Barbapapa', ];
-
 var randomHuttName = huttName[Math.floor(Math.random() * huttName.length)];
 
 
+var galaxyState = ['est en liesse', 'est en deuil', 'est aux abois', 'est en grand péril', 's\'abonne à Canal Satellite'];
+var randomGalaxyState = galaxyState[Math.floor(Math.random() * galaxyState.length)];
+
+
+var questObject = ['de l\'Holocron Sith', 'des plans de la treizième Etoile de la Mort', ];
+var randomQuestObject = questObject[Math.floor(Math.random() * questObject.length)];
+
+
+var questGoal = ['gagner instantanément la guerre'];
+var randomQuestGoal = questGoal[Math.floor(Math.random() * questGoal.length)];
+
+
+var order = ['le Premier Ordre', 'le Dernier Ordre', 'l\'Avant-Dernier Ordre', 'l\'Empire', 'la Rébellion', 'l\'Alliance Rebelle', 'la Résistance', 'les Sith', 'les Jedi'];
+var randomOrder = order[Math.floor(Math.random() * order.length)];
+
+var follower = ['des chasseurs de prime', 'ses remords'];
+var randomFollower = follower[Math.floor(Math.random() * follower.length)];
+
+var familyLink = [];
+var randomFamilyLInk = [];
 
 // PHRASES
 
 var phrase1 = randomMajorCharacter + ' se rend sur ' + randomPlanet + ' et y trouve ' + randomObject + ' ayant appartenu à ' + randomSecondaryCharacter + '.';
-
-var phrase2 = 'Au terme ' + randomAction + ', ' + randomMajorCharacter + ' découvre ' + randomSituation + ' ' + randomSecondaryCharacter + '.'
+var phrase2 = 'Au terme ' + randomAction + ', ' + randomMajorCharacter + ' découvre ' + randomSituation + ' ' + randomSecondaryCharacter + '.';
+var phrase3 = 'La Galaxie ' + randomGalaxyState + '.';
+var phrase4 = randomMajorCharacter + ' part en quête ' + randomQuestObject + ' permettant de ' + randomQuestGoal + '.';
+var phrase5 = 'La Force demande à ' + randomMajorCharacter + ' de détruire ' + randomObject + ' au bénéfice de ' + randomHuttName + ' le Hutt.';
+var phrase6 = 'La cantina de ' + randomPlanet + ' est assiégée par ' + randomOrder + ', sous les ordres de ' + randomSecondaryCharacter + '.';
+var phrase7 = 'Horreur ! ' + randomMajorCharacter + ', secrètement ressuscité par ' + randomSecondaryCharacter + ', menace ' + randomMajorCharacter + ' sur la planète ' + randomPlanet + '.';
+var phrase8 = 'Poursuivi par ' + randomFollower + ' impitoyables, ' + randomMajorCharacter + ' doit retrouver ' + randomObject + ' pour accomplir une prophétie de la Force.'
 
 var phraseConclusive = ['La paix reviendra-t-elle dans la galaxie ?', 'Kylo Ren mourra-t-il incel ?', 'Qu\'est-ce qui pourrait sauver l\'amour ? Et comment retrouver le goût de la vie ? Qui pourra remplacer le besoin par l\'envie ?'];
 
@@ -59,9 +71,18 @@ var randomPhraseConclusive = phraseConclusive[Math.floor(Math.random() * phraseC
 
 console.log(phrase1);
 console.log(phrase2);
+console.log(phrase3);
+console.log(phrase4);
+console.log(phrase5);
+console.log(phrase6);
+console.log(phrase7);
+console.log(phrase8);
 console.log(randomPhraseConclusive);
-/*
 
+
+
+
+/*
 
 'cousin germain du grand-oncle de'
 'ami d'enfance de la nourrice de'
@@ -70,12 +91,6 @@ console.log(randomPhraseConclusive);
 'la deuxième partie d'une dyade dans la Force avec'
 
 
-'La galaxie est ' + galaxyState
-_ part en quête _ permettant de _.
-La Force demande à _ de détruire _ au bénéfice de _ le Hutt.
-La cantina de _ est assiégée par _, sous les ordres de _.
-Horreur ! _, secrètement ressuscité par _, menace _ sur la planète _.
-Poursuivi par des chasseurs de primes impitoyables, _ doit retrouver _ pour accomplir une prophétie de la Force.
 Le droïde _ doit recompiler _ sur le vaisseau afin que les Jedi puissent _. Pendant ce temps, les Sith _ sur _.
 Mais _ découvre que _ s'est allié à _ dans sa quête désespérée de _, et d'ores et déjà, leurs troupes menacent de marcher sur _.
 _, _ s'engage dans la bataille et envoie des _ pour espionner _.
